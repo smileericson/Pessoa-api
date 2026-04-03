9f7f4989eaba9a365b195707846d2ad4acd07bb3
Silveira <ericson.silveira@thomsonreuters.com>
2026-04-02 21:44:16 -0300

    Pessoa api

---
+++ src/test/java/br/com/senac/pessoa_api/PessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.pessoa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
